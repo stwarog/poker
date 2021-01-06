2602ded0bb63c580aa1cdfa9733221d720494102
Sebastian Twaróg <contact@stwarog.com>
2021-01-06 14:44:51 +0100

    chip

---
+++ src/app/src/Game/Chip.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Game;
+
+
+use Webmozart\Assert\Assert;
+
+class Chip
+{
+    public const RED_25 = 25;
+    public const WHITE_50 = 50;
+    public const GREEN_100 = 100;
+    public const BLUE_500 = 500;
+    public const BLACK_1000 = 1000;
+
+    private int $value;
+
+    public function __construct(int $value)
+    {
+        Assert::greaterThanEq($value, self::RED_25, 'Amount must be greater than zero');
+        foreach ($this->toArray() as $allowedValue) {
+            if ($allowedValue > $value) {
+                break;
+            }
+            Assert::eq($value % $allowedValue, 0, sprintf('Amount must be dividable by: %s', implode(', ', $this->toArray())));
+        }
+        $this->value = $value;
+    }
+
+    private function toArray(): array
+    {
+        return [
+            self::RED_25,
+            self::WHITE_50,
+            self::GREEN_100,
+            self::BLUE_500,
+            self::BLACK_1000,
+        ];
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->value;
+    }
+}
---
+++ src/app/tests/Unit/Game/ChipTest.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Tests\Unit\Game;
+
+
+use App\Game\Chip;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class ChipTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider chip_new_with_allowed_value_dataProvider
+     *
+     * @param int $value
+     */
+    public function chip_new_with_allowed_value(int $value): void
+    {
+        // Given
+        $expected = $value;
+
+        // When
+        $c = new Chip($value);
+
+        // Then
+        $this->assertSame($expected, $c->getValue());
+    }
+
+    public function chip_new_with_allowed_value_dataProvider(): array
+    {
+        return [
+            'red 25'     => [25],
+            'white 50'   => [50],
+            'green 100'  => [100],
+            'blue 500'   => [500],
+            'black 1000' => [1000],
+        ];
+    }
+
+    /** @test */
+    public function chip__less_than_zero__throws_invalid_argument_exception(): void
+    {
+        // Except
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Amount must be greater than zero');
+
+        // When
+        new Chip(-1);
+    }
+
+    /**
+     * @test
+     * @dataProvider chip__value_not_dividable_by_values__dataProvider
+     */
+    public function chip__value_not_dividable_by_values__throws_invalid_argument_exception(int $amount): void
+    {
+        // Except
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Amount must be dividable by: 25, 50, 100, 500, 1000');
+
+        // When
+        new Chip($amount);
+    }
+
+    public function chip__value_not_dividable_by_values__dataProvider(): array
+    {
+        return [
+            'minimal allowed' => [26],
+            'big allowed'     => [100001],
+        ];
+    }
+}
